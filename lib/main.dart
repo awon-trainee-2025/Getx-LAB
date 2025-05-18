1a62a687b12b75e59091184a60f17db7c4acde79
Fanar <fnaralslmy38@gmail.com>
2025-05-18 12:02:15 +0300

    github is harder than this lab

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,10 @@ import 'package:get/get.dart';
 import 'package:getx_lab/views/page_1.dart';
 
 void main() {
-  // second route management way
   runApp(
     GetMaterialApp(
       home: const MyApp(),
-      getPages: [GetPage(name: '/page-1', page: () => Page1())],
+      getPages: [GetPage(name: '/page1', page: () => const Page1())],
     ),
   );
 }
@@ -15,6 +14,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,49 +22,66 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('Home page'),
+        title: Text('home page'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // old way
-            // 1- Navigator.push(
-            //   context,
-            //   MaterialPageRoute(builder: (context) => Page1()),
-            // );
-
-            // 2-
-            // Navigator.pushAndRemoveUntil(
-            //   context,
-            //   MaterialPageRoute(builder: (context) => Page1()),
-            //   (route) => false,
-            // );
-
-            // get way
-            // Get.to(Page1());
-            // 2- Get.offAll(Page1());
-
-            // 3- get pages
-            Get.toNamed('/page-1');
-
-            Get.offAllNamed('/page-1');
-          },
-          child: Text('Go to page 1'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                // Get.to(Page1());
+                //Get.offAll(Page1());
+                //Get.toNamed('page1');
+                Get.offAllNamed('/page1');
+              },
+              child: Text('go to page 1'),
+            ),
+          ],
         ),
-      ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- lib/views/page_1.dart
+++ lib/views/page_1.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_lab/main.dart';
 
 class Page1 extends StatelessWidget {
   const Page1({super.key});
@@ -7,17 +8,19 @@ class Page1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('page 1'),
-      ),
+      appBar: AppBar(title: const Text('Page 1')),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigator.pop(context);
-            Get.back();
-          },
-          child: Text('Back to hemo page'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                // Get.back();
+                Get.offAll(MyApp());
+              },
+              child: Text('back to home page'),
+            ),
+          ],
         ),
       ),
     );
