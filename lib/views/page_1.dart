8cc33ad82be1e11cf4b5541a0af8fba3ee2a7e17
Jasser230 <gaseraz150@gmail.com>
2025-05-18 12:17:31 +0300

    Jasser

---
+++ lib/views/page_1.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class Page1 extends StatelessWidget {
+  const Page1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('page 1'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Get.back();
+          },
+          child: Text('Back to hemo page'),
+        ),
+      ),
+    );
+  }
+}
