1f88fea20a84dd1c73f03cf5d7efdc089fae3b73
Whaj Alyasi <whajalyasi@gmail.com>
2025-05-18 12:08:20 +0300

    getx lab wahaj

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,18 @@ import 'package:get/get.dart';
 import 'package:getx_lab/views/page_1.dart';
 
 void main() {
-  // second route management way
   runApp(
     GetMaterialApp(
-      home: const MyApp(),
-      getPages: [GetPage(name: '/page-1', page: () => Page1())],
-    ),
+    home:const MyApp(),
+  getPages: [GetPage(name: '/page-1', page:()=> Page1())],
+  ),
   );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,39 +32,21 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('Home page'),
+        title: Text('my home page'),
       ),
       body: Center(
         child: ElevatedButton(
-          onPressed: () {
-            // old way
-            // 1- Navigator.push(
-            //   context,
-            //   MaterialPageRoute(builder: (context) => Page1()),
-            // );
-
-            // 2-
-            // Navigator.pushAndRemoveUntil(
-            //   context,
-            //   MaterialPageRoute(builder: (context) => Page1()),
-            //   (route) => false,
-            // );
-
-            // get way
-            // Get.to(Page1());
-            // 2- Get.offAll(Page1());
-
-            // 3- get pages
-            Get.toNamed('/page-1');
-
-            Get.offAllNamed('/page-1');
-          },
-          child: Text('Go to page 1'),
-        ),
+          onPressed:(){
+           // Get.to(Page1());
+           Get.toNamed('/page-1');
+          //  Get.offAllNamed('/page-1');
+          }, 
+          child: Text('go to page 1'),
+          ),
       ),
-    );
+      );
   }
 }
